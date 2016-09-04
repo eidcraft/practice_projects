9ea113df1d8b07c746f43e198c6f518bf49da3f9
eidcraft <anton.zheltouhov@gmail.com>
2016-09-04 21:28:53 +0300

    Create check_if_palindrome function

--- check_if_palindrome/main.cpp
+++ check_if_palindrome/main.cpp
@@ -7,17 +7,23 @@
 using namespace std::string_literals;
 
 
-bool check_if_palindrome(const std::string& str)
+bool check_if_palindrome(std::string& str)
 {
-  return true;
+  if (str.size() < 2) return true;
+
+  auto offset = (int)std::floor((double)str.size() / 2);
+  auto left_midl_it = begin(str) + offset;
+  auto right_midl_it = rbegin(str) + offset;
+
+  return std::equal(begin(str), left_midl_it, rbegin(str), right_midl_it);
 }
 
 int main()
 {
-  auto palindrome     = "abcdcba"s;
+  auto palindrome = "abcdcba"s;
   auto not_palindrome = "abcdefg"s;
 
-  std::cout << palindrome << " is palindrome? - " << 
+  std::cout << palindrome << " is palindrome? - " <<
     check_if_palindrome(palindrome) << std::endl;
 
   std::cout << not_palindrome << " is palindrome? - " <<
