d28fc163ca27c935657fedd1b689efeea982abce
eidcraft <anton.zheltouhov@gmail.com>
2016-09-05 11:28:07 +0300

    Create count_words_in_a_string function

--- count_words_in_a_string/count_words_in_a_string.cpp
+++ count_words_in_a_string/count_words_in_a_string.cpp
@@ -2,18 +2,31 @@
 
 #include <iostream>
 #include <string>
+#include <boost/regex.hpp>
 
 using namespace std::string_literals;
 
 
 size_t count_words_in_a_string(std::string& str)
 {
-  return 0;
+  boost::sregex_iterator regex_it{
+    begin(str), end(str), boost::regex{"\\w+"}};
+  boost::sregex_iterator regex_end_it;
+
+  size_t counter = 0;
+  while (regex_it != regex_end_it)
+  {
+    counter++;
+    regex_it++;
+  }
+
+  return counter;
 }
+
 
 int main()
 {
-  auto str = "string from four words"s;
+  auto str = "AAAA 12222 BBBBB 44455"s;
 
   std::cout << "string \"" << str << "\" contains "
             << count_words_in_a_string(str) << " words" << std::endl;
