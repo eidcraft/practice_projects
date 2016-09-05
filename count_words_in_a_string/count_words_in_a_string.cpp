ed868a3c6d30cbd6e48331c654588f5224a9b3fc
eidcraft <anton.zheltouhov@gmail.com>
2016-09-05 11:40:48 +0300

    Refactor count_words_in_a_string function

--- count_words_in_a_string/count_words_in_a_string.cpp
+++ count_words_in_a_string/count_words_in_a_string.cpp
@@ -9,18 +9,9 @@ using namespace std::string_literals;
 
 size_t count_words_in_a_string(std::string& str)
 {
-  boost::sregex_iterator regex_it{
-    begin(str), end(str), boost::regex{"\\w+"}};
-  boost::sregex_iterator regex_end_it;
-
-  size_t counter = 0;
-  while (regex_it != regex_end_it)
-  {
-    counter++;
-    regex_it++;
-  }
-
-  return counter;
+  return std::distance(
+    boost::sregex_iterator{begin(str), end(str), boost::regex{"\\w+"}},
+    boost::sregex_iterator{});
 }
 
 
